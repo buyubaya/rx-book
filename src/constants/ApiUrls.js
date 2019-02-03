b13bc76e8970211cebe53834812d334f50b470da
buyubaya <hihihi_caigikia@yahoo.com>
2019-02-03 20:11:04 +0700

    'HELLO'

--- src/constants/ApiUrls.js
+++ src/constants/ApiUrls.js
@@ -1,5 +1,5 @@
-export const BASE_API_URL = 'http://localhost:3000';
-// export const BASE_API_URL = 'https://nodejs-book-api.herokuapp.com';
+// export const BASE_API_URL = 'http://localhost:3000';
+export const BASE_API_URL = 'https://nodejs-book-api.herokuapp.com';
 export const BOOK_API_URL = `${BASE_API_URL}/api/product`;
 export const CATEGORY_API_URL = `${BASE_API_URL}/api/category`;
 export const AUTHOR_API_URL = `${BASE_API_URL}/api/author`;
